d51cd361536418662fa182bcd8bdda68cf6c18d3
Ozzie <ayon1524@gmail.com>
2023-10-02 22:08:46 +0300

    fix methods names

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -2,8 +2,6 @@ package com.trendyol.shipment;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
@@ -18,25 +16,25 @@ public class Basket {
             return null;
         }
 
-        return incrementAndGetShipmentSize();
+        return findShipmentSize();
     }
 
-    private ShipmentSize findLargestShipmentSize() {
-        return products.stream()
-                .map(Product::getSize)
-                .max(ShipmentSize::compareTo).orElseThrow();
-    }
-
-    private ShipmentSize incrementAndGetShipmentSize(){
+    private ShipmentSize findShipmentSize(){
        ShipmentSize shipmentSize = products.stream().collect(groupingBy(Product::getSize, counting())).entrySet()
                 .stream().filter(s -> s.getValue() >= thresholdValue).map(Map.Entry::getKey).findAny().orElse(null);
 
        if(products.size() < thresholdValue || shipmentSize == null){
-        return findLargestShipmentSize();
+        return findLargest();
        }
 
        return shipmentSize.getBiggerSize();
 
+    }
+
+    private ShipmentSize findLargest() {
+        return products.stream()
+                .map(Product::getSize)
+                .max(ShipmentSize::compareTo).orElseThrow();
     }
 
     private boolean isBasketEmpty() {
